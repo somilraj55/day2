8075615f4af70db925da891d5f9c85c1cbec3d01
Somil <somilraj55@gmail.com>
2026-02-08 02:19:23 +0530

    package.json added

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
-const app = express() // server instance create kar chuke hai;
+const app = express(); // server instance create kar chuke hai;
 
-app.get('/',(req,res)=>{
-    res.send("Hello World")
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.get("/about",(req,res)=>{
-    res.send("This is a about page ");
+app.get("/about", (req, res) => {
+  res.send("This is a about page ");
 });
 
-app.get("/home",(req,res)=>{
-    res.send("This is a home page");
-})
+app.get("/home", (req, res) => {
+  res.send("This is a home page");
+});
 
-app.listen(3000); // server start karna;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
